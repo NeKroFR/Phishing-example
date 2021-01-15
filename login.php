5d29c062b7e5fd31f7afa92d0aaf97238a858c78
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2021-01-15 19:01:07 +0100

    Update login.php

--- login.php
+++ login.php
@@ -21,42 +21,18 @@
                 border: outset black;
                 width: 250px;
                 height: 100px;
-
-
-
             }
         </style>
-        <center>
-            <h1 style="text-align center">PHISHING don't login!!</h1>
-            <div id="div">
-                <br>
-                    <form method="post">
-                        <input type="text" name="Username" id="name" placeholder="Nom d'utilisateur"><br>
-                        <input type="text" name="Password" id="password"placeholder="Mot de passe"><br>
-                        <input type="submit" name="login" id="login">
-                    </form>
+            
+        <h1 style="text-align center">LOGIN</h1>
+        <div id="div">
+            <br>
+            <form method="post">
+                <input type="text" name="username" id="name" placeholder="Nom d'utilisateur"><br>
+                <input type="text" name="password" id="password"placeholder="Mot de passe"><br>
+                <input type="submit" name="login" id="login">
+            </form>
             </div>
-        </center>
-
-
-        <?php
-
-            if(isset($_POST['formsend'])){
-                extract($_POST);
-
-                include 'database.php';
-                global $db;
-
-                $q = $db ->prepare("INSERT INTO phishing(Username,Password) VALUES(:Username,Password)");
-                $q ->execute([
-                    'Username' => $Username,
-                    'Password' => $Password
-
-                ])
-            }
-
-        
-        ?>
     </body>
 
 
